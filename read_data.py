6499fa93fc159f7e76738994b794cc38490ac775
culuc <luc.seiler@me.com>
2020-05-25 16:21:50 +0200

    clean-up

--- read_data.py
+++ read_data.py
@@ -200,34 +200,32 @@ def select_phrases_from_df(df,newtop500,by_index,dropna=True):
 
 
 #%%
-term5_tf, term5top500_tf = compute_tf_idf(dfoverall5,dfbyparty,500)
+term5_tf, term5top500_tf = compute_tf_idf(dfoverall5,dfbyparty5,500)
 
 # %%
 term5_top500_bySpeakerParty = select_phrases_from_df(dfbypartyspeaker5,term5top500_tf,['Speaker Party','Speaker'])
 term5_top500_bySpeakerParty.shape
 
 
-# %% include phrases mentioned at least 20 times
+# %% include phrases mentioned at least 20 times -> 12'433 w
 dfoverall5cap20 = dfoverall5[dfoverall5.Counts >= 20]
 
-# %% include phrases mentioned at least 20 times
+# %% include phrases mentioned at least 50 times -> 3'054 w
+dfoverall5cap50 = dfoverall5[dfoverall5.Counts >= 50]
+
+# %% include phrases mentioned at least 100 times -> 991 w
 dfoverall5cap100 = dfoverall5[dfoverall5.Counts >= 100]
+
 #%%
-term5_cap20_bySpeakerParty_long = select_phrases_from_df(dfbypartyspeaker5,dfoverall5cap20,['Speaker Party','Speaker'],dropna=False)
+term5_cap20_bySpeakerParty = select_phrases_from_df(dfbypartyspeaker5,dfoverall5cap20,['Speaker Party','Speaker'])
+term5_cap20_bySpeakerParty.shape
+#%%
+term5_cap50_bySpeakerParty = select_phrases_from_df(dfbypartyspeaker5,dfoverall5cap50,['Speaker Party','Speaker'])
+term5_cap50_bySpeakerParty.shape
+#%%
+term5_cap100_bySpeakerParty = select_phrases_from_df(dfbypartyspeaker5,dfoverall5cap100,['Speaker Party','Speaker'])
+term5_cap100_bySpeakerParty.shape
 
-# %%
-term5_cap20_bySpeakerParty_short = select_phrases_from_df(dfbypartyspeaker5,dfoverall5cap20,['Speaker Party','Speaker'],dropna=True)
-
-# %% save results
-term5_top500_bySpeakerParty.to_csv('./term5/term5_top500_bySpeakerParty.csv')
-
-# %% save results 2
-term5_cap20_bySpeakerParty_short.to_csv('./term5/term5_cap20_bySpeakerParty_short.csv')
-term5_cap20_bySpeakerParty_long.to_csv('./term5/term5_cap20_bySpeakerParty_long.csv')
-
-
-# %%
-term5_cap20_bySpeakerParty_short.sum(axis=1).to_csv('./term5_test.csv')
 
 # %%
 def share(series):
@@ -252,13 +250,35 @@ def make_share(df,scale=True):
     df_final = pd.concat([df[['Speaker','Speaker Party']],df2],axis=1)
 
     return df_final
-#%%
-term5_cap20_scaled_long = make_share(term5_cap20_bySpeakerParty_long)
 
-#%%
-term5_cap20_scaled_short = make_share(term5_cap20_bySpeakerParty_short)
+# %% tfidf top 500
+term5_top500_bySpeakerParty_scaled = make_share(term5_top500_bySpeakerParty)
+term5_top500_bySpeakerParty_share = make_share(term5_top500_bySpeakerParty, scale=False)
+#%% cap20
+term5_cap20_scaled = make_share(term5_cap20_bySpeakerParty)
+term5_cap20_share = make_share(term5_cap20_bySpeakerParty, scale=False)
 
+#%% cap50
+term5_cap50_scaled = make_share(term5_cap50_bySpeakerParty)
+term5_cap50_share = make_share(term5_cap50_bySpeakerParty, scale=False)
+
+#%% cap 100
+term5_cap100_scaled = make_share(term5_cap100_bySpeakerParty)
+term5_cap100_share = make_share(term5_cap100_bySpeakerParty, scale=False)
+
+# %% save results
+term5_top500_bySpeakerParty.to_csv('./term5/tfidf/term5_top500_bySpeakerParty.csv')
+term5_top500_bySpeakerParty_scaled.to_csv('./term5/tfidf/term5_top500_scaled.csv')
+term5_top500_bySpeakerParty_share.to_csv('./term5/tfidf/term5_top500_share.csv')
 # %%
-term5_cap20_scaled_short.to_csv('./term5/term5_cap20_scaled_short.csv')
-
+term5_cap20_bySpeakerParty.to_csv('./term5/cap/term5_cap20.csv')
+term5_cap20_scaled.to_csv('./term5/cap/term5_cap20_scaled.csv')
+term5_cap20_share.to_csv('./term5/cap/term5_cap20_share.csv')
 # %%
+term5_cap50_bySpeakerParty.to_csv('./term5/cap/term5_cap50.csv')
+term5_cap50_scaled.to_csv('./term5/cap/term5_cap50_scaled.csv')
+term5_cap50_share.to_csv('./term5/cap/term5_cap50_share.csv')
+# %%
+term5_cap100_bySpeakerParty.to_csv('./term5/cap/term5_cap100.csv')
+term5_cap100_scaled.to_csv('./term5/cap/term5_cap100_scaled.csv')
+term5_cap100_share.to_csv('./term5/cap/term5_cap100_share.csv')
