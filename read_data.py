79c8dc0143b53173ec47ee98012114218f91cdbb
culuc <luc.seiler@me.com>
2020-05-25 18:19:31 +0200

    make files with top 1000 tf-idf

--- read_data.py
+++ read_data.py
@@ -202,9 +202,15 @@ def select_phrases_from_df(df,newtop500,by_index,dropna=True):
 #%%
 term5_tf, term5top500_tf = compute_tf_idf(dfoverall5,dfbyparty5,500)
 
+#%%
+term5_tf, term5top1000_tf = compute_tf_idf(dfoverall5,dfbyparty5,1000)
+
 # %%
 term5_top500_bySpeakerParty = select_phrases_from_df(dfbypartyspeaker5,term5top500_tf,['Speaker Party','Speaker'])
 term5_top500_bySpeakerParty.shape
+# %%
+term5_top1000_bySpeakerParty = select_phrases_from_df(dfbypartyspeaker5,term5top1000_tf,['Speaker Party','Speaker'])
+term5_top1000_bySpeakerParty.shape
 
 
 # %% include phrases mentioned at least 20 times -> 12'433 w
@@ -254,6 +260,9 @@ def make_share(df,scale=True):
 # %% tfidf top 500
 term5_top500_bySpeakerParty_scaled = make_share(term5_top500_bySpeakerParty)
 term5_top500_bySpeakerParty_share = make_share(term5_top500_bySpeakerParty, scale=False)
+# %% tfidf top 1000
+term5_top1000_bySpeakerParty_scaled = make_share(term5_top1000_bySpeakerParty)
+term5_top1000_bySpeakerParty_share = make_share(term5_top1000_bySpeakerParty, scale=False)
 #%% cap20
 term5_cap20_scaled = make_share(term5_cap20_bySpeakerParty)
 term5_cap20_share = make_share(term5_cap20_bySpeakerParty, scale=False)
@@ -271,14 +280,18 @@ term5_top500_bySpeakerParty.to_csv('./term5/tfidf/term5_top500_bySpeakerParty.cs
 term5_top500_bySpeakerParty_scaled.to_csv('./term5/tfidf/term5_top500_scaled.csv')
 term5_top500_bySpeakerParty_share.to_csv('./term5/tfidf/term5_top500_share.csv')
 # %%
-term5_cap20_bySpeakerParty.to_csv('./term5/cap/term5_cap20.csv')
+term5_top1000_bySpeakerParty.to_csv('./term5/tfidf/term5_top1000_bySpeakerParty.csv')
+term5_top1000_bySpeakerParty_scaled.to_csv('./term5/tfidf/term5_top1000_scaled.csv')
+term5_top1000_bySpeakerParty_share.to_csv('./term5/tfidf/term5_top1000_share.csv')
+# %%
+term5_cap20_bySpeakerParty.to_csv('./term5/cap/term5_cap20_bySpeakerParty.csv')
 term5_cap20_scaled.to_csv('./term5/cap/term5_cap20_scaled.csv')
 term5_cap20_share.to_csv('./term5/cap/term5_cap20_share.csv')
 # %%
-term5_cap50_bySpeakerParty.to_csv('./term5/cap/term5_cap50.csv')
+term5_cap50_bySpeakerParty.to_csv('./term5/cap/term5_cap50_bySpeakerParty.csv')
 term5_cap50_scaled.to_csv('./term5/cap/term5_cap50_scaled.csv')
 term5_cap50_share.to_csv('./term5/cap/term5_cap50_share.csv')
 # %%
-term5_cap100_bySpeakerParty.to_csv('./term5/cap/term5_cap100.csv')
+term5_cap100_bySpeakerParty.to_csv('./term5/cap/term5_cap100_bySpeakerParty.csv')
 term5_cap100_scaled.to_csv('./term5/cap/term5_cap100_scaled.csv')
 term5_cap100_share.to_csv('./term5/cap/term5_cap100_share.csv')
