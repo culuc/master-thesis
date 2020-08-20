2127c2ce0e65b507ed78607bfd7e7040650cdd1f
culuc <luc.seiler@me.com>
2020-08-20 12:36:57 +0200

    read data update

---
+++ Read Data/filter_consecutive_speakers.py
@@ -0,0 +1,32 @@
+#%%
+import sys
+sys.path.append('../Modules')
+#%%
+import modules as m
+import pandas as pd
+# %%
+# import importlib
+#%%
+# importlib.reload(m)
+
+cons_speakers = pd.read_csv('../Data/lookup_files/speaker_terms_servetime_consecutive_years.csv')
+cons_speakers = set(cons_speakers.Speaker)
+# %mkdir '../../interim/consecutive_speakers'
+for i in range(1,6):
+    byPartySpeaker = pd.read_pickle('../../interim/t'+str(i)+'_byPartySpeaker.pkl')
+    byPartySpeaker = byPartySpeaker[byPartySpeaker.Speaker.isin(cons_speakers)]
+
+    byParty = byPartySpeaker.groupby('Speaker Party').sum().sort_values(['Speaker Party','Counts'])
+
+    byPartySpeaker.to_pickle('../../interim/consecutive_speakers/t'+str(i)+'_byPartySpeaker.pkl')
+    byParty.to_pickle('../../interim/consecutive_speakers/t'+str(i)+'_byParty.pkl')
+
+
+new[new.Speaker.isin(cons_speakers)]
+
+old = pd.read_pickle('../../interim/t1_byPartySpeaker.pkl')
+new = pd.read_pickle('../../interim/consecutive_speakers/t1_byPartySpeaker.pkl')
+
+new
+set(old.Speaker)- cons_speakers
+set(new.Speaker)-cons_speakers
--- Read Data/read_data_term_5.py
+++ Read Data/read_data_term_5.py
@@ -19,28 +19,28 @@ alldata = m.read_data(path)
 alldata = m.compute_term(alldata,elections)
 
 # %%
-data1 = m.preprocess_data(alldata, 'Term', 1)
+data5 = m.preprocess_data(alldata, 'Term', 5)
 # del alldata
 # %%
-data1.drop(columns=['Date','Session','Term','Topic','Language'],inplace=True)
+data5.drop(columns=['Date','Session','Term','Topic','Language'],inplace=True)
 
 # %%
-overall = m.collect_all(data1)
+overall = m.collect_all(data5)
 #%%
-by_party = m.collect_by(data1, ['Speaker Party'])
+by_party = m.collect_by(data5, ['Speaker Party'])
 #%%
-by_party_speaker = m.collect_by(data1, ['Speaker Party','Speaker'])
+by_party_speaker = m.collect_by(data5, ['Speaker Party','Speaker'])
 
 
 #%% save to overall
-overall.to_csv('../../interim/t1_overall.csv')
-overall.to_pickle('../../interim/t1_overall.pkl')
-overall.to_json('../../interim/t1_overall.json')
+overall.to_csv('../../interim/t5_overall.csv')
+overall.to_pickle('../../interim/t5_overall.pkl')
+overall.to_json('../../interim/t5_overall.json')
 #%% save to byparty
-by_party.to_csv('../../interim/t1_byParty.csv')
-by_party.to_pickle('../../interim/t1_byParty.pkl')
-by_party.to_json('../../interim/t1_byParty.json')
+by_party.to_csv('../../interim/t5_byParty.csv')
+by_party.to_pickle('../../interim/t5_byParty.pkl')
+by_party.to_json('../../interim/t5_byParty.json')
 #%% save to by party speaker
-by_party_speaker.to_csv('../../interim/t1_byPartySpeaker.csv')
-by_party_speaker.to_pickle('../../interim/t1_byPartySpeaker.pkl')
-by_party_speaker.to_json('../../interim/t1_byPartySpeaker.json')
+by_party_speaker.to_csv('../../interim/t5_byPartySpeaker.csv')
+by_party_speaker.to_pickle('../../interim/t5_byPartySpeaker.pkl')
+by_party_speaker.to_json('../../interim/t5_byPartySpeaker.json')
