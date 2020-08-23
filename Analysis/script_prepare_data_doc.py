21f7d667871f42fb24831c1ab16e71f7f6139535
culuc <luc.seiler@me.com>
2020-08-23 18:09:31 +0200

    index

--- Analysis/script_prepare_data_doc.py
+++ Analysis/script_prepare_data_doc.py
@@ -41,11 +41,11 @@ for i in range(1,l_in):
     term1_topN_bySpeakerParty_share = m.make_share(term1_topN_bySpeakerParty, scale=False)
 
     # %% save result
-    print(sys.argv[l_in+2+(i-1)*3])
-    term1_topN_bySpeakerParty.to_csv(sys.argv[l_in+2+(i-1)*3])
-    term1_topN_bySpeakerParty_scaled.to_csv(sys.argv[l_in+3+(i-1)*3])
-    term1_topN_bySpeakerParty_share.to_csv(sys.argv[l_in+4+(i-1)*3])
+    print(sys.argv[l_in-1+(i-1)*3])
+    term1_topN_bySpeakerParty.to_csv(sys.argv[l_in-1+(i-1)*3])
+    term1_topN_bySpeakerParty_scaled.to_csv(sys.argv[l_in+(i-1)*3])
+    term1_topN_bySpeakerParty_share.to_csv(sys.argv[l_in+1+(i-1)*3])
 
     # %% save interim results
-    term1_tf.to_pickle(sys.argv[l_in+5+(i-1)*3])
-    term1topN_tf.to_csv(sys.argv[l_in+6+(i-1)*3])
+    term1_tf.to_pickle(sys.argv[l_in+2+(i-1)*3])
+    term1topN_tf.to_csv(sys.argv[l_in+3+(i-1)*3])
