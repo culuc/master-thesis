87887336e8ef2c9f72d5a99ad71278ecfe866a9a
culuc <luc.seiler@me.com>
2020-08-29 19:14:20 +0200

    test

--- Analysis/script.R
+++ Analysis/script.R
@@ -23,8 +23,9 @@ out_models  <- args[7]
 out_acc <- args[8]
 out_acc2 <- args[9]
 # out_models2  <- args[5]
-
+in_data <- argsv[2:6]
 # source(in_lib)
+l <- c(1,2,3)
 
 # Load Model and Data #
 print('Loading Objects in to R Session')
@@ -33,19 +34,19 @@ print(in_data2)
 print(in_data5)
 print(out_models)
 print(Cstack_info())
-df1         <- read_csv(in_data1)%>%select(-c('X1','Speaker'))
-df2         <- read_csv(in_data2)%>%select(-c('X1','Speaker'))
-df3         <- read_csv(in_data3)%>%select(-c('X1','Speaker'))
-df4         <- read_csv(in_data4)%>%select(-c('X1','Speaker'))
-df5         <- read_csv(in_data5)%>%select(-c('X1','Speaker'))
-# df1         <- read_csv(in_data[2])%>%select(-c('X1'))
-model_info <- fromJSON(file = in_model)
-
-colnames(df1) <- make.names(colnames(df1), unique=TRUE)
-colnames(df2) <- make.names(colnames(df2), unique=TRUE)
-colnames(df3) <- make.names(colnames(df3), unique=TRUE)
-colnames(df4) <- make.names(colnames(df4), unique=TRUE)
-colnames(df5) <- make.names(colnames(df5), unique=TRUE)
+# df1         <- read_csv(in_data1)%>%select(-c('X1','Speaker'))
+# df2         <- read_csv(in_data2)%>%select(-c('X1','Speaker'))
+# df3         <- read_csv(in_data3)%>%select(-c('X1','Speaker'))
+# df4         <- read_csv(in_data4)%>%select(-c('X1','Speaker'))
+# df5         <- read_csv(in_data5)%>%select(-c('X1','Speaker'))
+# # df1         <- read_csv(in_data[2])%>%select(-c('X1'))
+# model_info <- fromJSON(file = in_model)
+#
+# colnames(df1) <- make.names(colnames(df1), unique=TRUE)
+# colnames(df2) <- make.names(colnames(df2), unique=TRUE)
+# colnames(df3) <- make.names(colnames(df3), unique=TRUE)
+# colnames(df4) <- make.names(colnames(df4), unique=TRUE)
+# colnames(df5) <- make.names(colnames(df5), unique=TRUE)
 
 # Unpack Model Info #
 print('Unpacking JSON data')
@@ -69,81 +70,100 @@ print(paste('Working with', nrow(df1), 'rows of data', sep = " "))
 # Estimate Models #
 print('Estimating Models')
 print('Model 1...')
+caret_model <- rep(NaN,5)
 
-caret_1 <- train(
-  Speaker.Party ~ .,
-  df1,
-  method = method,
-  tuneGrid = tuneGrid,
-  trControl = trainControl(
-      method = "cv",
-      number = 10,
-      verboseIter = TRUE
+
+for (i in 1:5){
+    df         <- read_csv(in_data[i])%>%select(-c('X1','Speaker'))
+    colnames(df) <- make.names(colnames(df), unique=TRUE)
+    caret_model[i] <- train(
+      Speaker.Party ~ .,
+      df,
+      method = method,
+      tuneGrid = tuneGrid,
+      trControl = trainControl(
+          method = "cv",
+          number = 10,
+          verboseIter = TRUE
+        )
     )
-)
-caret_2 <- train(
-  Speaker.Party ~ .,
-  df2,
-  method = method,
-  tuneGrid = tuneGrid,
-  trControl = trainControl(
-    method = "cv",
-    number = 10,
-    verboseIter = TRUE
-  )
-)
-caret_3 <- train(
-  Speaker.Party ~ .,
-  df3,
-  method = method,
-  tuneGrid = tuneGrid,
-  trControl = trainControl(
-    method = "cv",
-    number = 10,
-    verboseIter = TRUE
-  )
-)
-caret_4 <- train(
-  Speaker.Party ~ .,
-  df4,
-  method = method,
-  tuneGrid = tuneGrid,
-  trControl = trainControl(
-    method = "cv",
-    number = 10,
-    verboseIter = TRUE
-  )
-)
-caret_5 <- train(
-  Speaker.Party ~ .,
-  df5,
-  method = method,
-  tuneGrid = tuneGrid,
-  trControl = trainControl(
-    method = "cv",
-    number = 10,
-    verboseIter = TRUE
-  )
-)
-summary(caret_1)
+}
+
+#
+# caret_1 <- train(
+#   Speaker.Party ~ .,
+#   df1,
+#   method = method,
+#   tuneGrid = tuneGrid,
+#   trControl = trainControl(
+#       method = "cv",
+#       number = 10,
+#       verboseIter = TRUE
+#     )
+# )
+# caret_2 <- train(
+#   Speaker.Party ~ .,
+#   df2,
+#   method = method,
+#   tuneGrid = tuneGrid,
+#   trControl = trainControl(
+#     method = "cv",
+#     number = 10,
+#     verboseIter = TRUE
+#   )
+# )
+# caret_3 <- train(
+#   Speaker.Party ~ .,
+#   df3,
+#   method = method,
+#   tuneGrid = tuneGrid,
+#   trControl = trainControl(
+#     method = "cv",
+#     number = 10,
+#     verboseIter = TRUE
+#   )
+# )
+# caret_4 <- train(
+#   Speaker.Party ~ .,
+#   df4,
+#   method = method,
+#   tuneGrid = tuneGrid,
+#   trControl = trainControl(
+#     method = "cv",
+#     number = 10,
+#     verboseIter = TRUE
+#   )
+# )
+# caret_5 <- train(
+#   Speaker.Party ~ .,
+#   df5,
+#   method = method,
+#   tuneGrid = tuneGrid,
+#   trControl = trainControl(
+#     method = "cv",
+#     number = 10,
+#     verboseIter = TRUE
+#   )
+# )
+# summary(caret_1)
 # summary(caret_2)
 
 
 # pack it into a list #
 model_list <- list(
-                   model1    = caret_1,
-                   model2    = caret_2,
-                   model3    = caret_3,
-                   model4    = caret_4,
-                   model5    = caret_5
+                   model1    = caret[1],
+                   model2    = caret[2],
+                   model3    = caret[3],
+                   model4    = caret[4],
+                   model5    = caret[5]
                    )
 
 acc_list <- list(
-                  term1    = mean(caret_1$resample$Accuracy),
-                  term2    = mean(caret_2$resample$Accuracy),
-                  term3    = mean(caret_3$resample$Accuracy),
-                  term4    = mean(caret_4$resample$Accuracy),
-                  term5    = mean(caret_5$resample$Accuracy)
+                  term1    = mean(caret[1]$resample$Accuracy),
+                  term2    = mean(caret[2]$resample$Accuracy),
+                  term3    = mean(caret[3]$resample$Accuracy),
+                  term4    = mean(caret[4]$resample$Accuracy),
+                  term5    = mean(caret[5]$resample$Accuracy)
                   )
 
 # Save Output #
