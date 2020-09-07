8467f0694e7d34470606349675477499bd090d01
culuc <luc.seiler@me.com>
2020-09-07 23:01:50 +0200

    fix fixed analysis

--- Analysis/script_fixed_terms.py
+++ Analysis/script_fixed_terms.py
@@ -23,22 +23,22 @@ parties4 = [p1,p2,p3,p4]
 # pp1 = pd.read_pickle('../Data/lookup_files/procedural_phrases.pkl')
 # pp2 = pd.read_pickle('procedural_phrases_SpSvpDistinct.pkl')
 
-for i in range(1,6):
 
-    dfoverall = pd.read_pickle(sys.argv[2*i+3])
-    #%%
-    dfbyparty = pd.read_pickle(sys.argv[2*i+4])
 
-    dfbyparty_filt = dfbyparty[dfbyparty.Phrase.isin(pp).apply(lambda x: not x)]
+dfoverall = pd.read_pickle(sys.argv[5])
+#%%
+dfbyparty = pd.read_pickle(sys.argv[6])
 
-    if N is not None:
-        dfbyparty_filt = dfbyparty_filt[dfbyparty_filt.Phrase.isin(parties4[0:N])]
-    #%%
-    if indiv ==1:
-        term1_tf, term1topN_tf = m.compute_tf_idf_new(dfbyparty_filt,'Speaker Party',n)
-    else:
-        dfoverall = dfbyparty_filt.groupby('Phrase').sum()
-        term1_tf, term1topN_tf = m.compute_tf_idf_old(dfoverall,dfbyparty_filt,500)
-    # %% save interim results
-    term1_tf.to_pickle(sys.argv[2*i+13])
-    term1topN_tf.to_csv(sys.argv[2*i+14])
+dfbyparty_filt = dfbyparty[dfbyparty.Phrase.isin(pp).apply(lambda x: not x)]
+
+if N is not None:
+    dfbyparty_filt = dfbyparty_filt[dfbyparty_filt.Phrase.isin(parties4[0:N])]
+#%%
+if indiv ==1:
+    term1_tf, term1topN_tf = m.compute_tf_idf_new(dfbyparty_filt,'Speaker Party',n)
+else:
+    dfoverall = dfbyparty_filt.groupby('Phrase').sum()
+    term1_tf, term1topN_tf = m.compute_tf_idf_old(dfoverall,dfbyparty_filt,500)
+# %% save interim results
+term1_tf.to_pickle(sys.argv[7])
+term1topN_tf.to_csv(sys.argv[8])
