adb7f029127daa5a5c03b11f90ead534dfac1223
culuc <luc.seiler@me.com>
2020-09-07 17:20:35 +0200

    fix

--- Analysis/script_fixed_terms.py
+++ Analysis/script_fixed_terms.py
@@ -6,13 +6,11 @@ import modules as m
 import pandas as pd
 
 #%% snakemake.input[0]
-dfoverall = pd.read_pickle(sys.argv[1])
-#%%
-dfbyparty = pd.read_pickle(sys.argv[2])
-pp = pd.read_pickle(sys.argv[3])
-indiv=sys.argv[4]
-n=int(sys.argv[5])
-N = int(sys.argv[6])
+pp = pd.read_pickle(sys.argv[1])
+indiv=sys.argv[2]
+n=int(sys.argv[3])
+N = int(sys.argv[4])
+
 
 #%%
 p1 = 'Sozialdemokratische Partei der Schweiz (SP)'
@@ -24,16 +22,23 @@ parties4 = [p1,p2,p3,p4]
 # # dfbypartyspeaker = pd.read_pickle(sys.argv[3])
 # pp1 = pd.read_pickle('../Data/lookup_files/procedural_phrases.pkl')
 # pp2 = pd.read_pickle('procedural_phrases_SpSvpDistinct.pkl')
-dfbyparty_filt = dfbyparty[dfbyparty.Phrase.isin(pp).apply(lambda x: not x)]
 
-if N is not None:
-    dfbyparty_filt = dfbyparty_filt[dfbyparty_filt.Phrase.isin(parties4[0:N])]
-#%%
-if indiv ==1:
-    term1_tf, term1topN_tf = m.compute_tf_idf_new(dfbyparty_filt,'Speaker Party',n)
-else:
-    dfoverall = dfbyparty_filt.groupby('Phrase').sum()
-    term1_tf, term1topN_tf = m.compute_tf_idf(dfoverall,dfbyparty_filt,500)
-# %% save interim results
-term1_tf.to_pickle(sys.argv[7])
-term1topN_tf.to_csv(sys.argv[8])
+for i in range(1,6):
+
+    dfoverall = pd.read_pickle(sys.argv[4*i+1])
+    #%%
+    dfbyparty = pd.read_pickle(sys.argv[4*i+2])
+
+    dfbyparty_filt = dfbyparty[dfbyparty.Phrase.isin(pp).apply(lambda x: not x)]
+
+    if N is not None:
+        dfbyparty_filt = dfbyparty_filt[dfbyparty_filt.Phrase.isin(parties4[0:N])]
+    #%%
+    if indiv ==1:
+        term1_tf, term1topN_tf = m.compute_tf_idf_new(dfbyparty_filt,'Speaker Party',n)
+    else:
+        dfoverall = dfbyparty_filt.groupby('Phrase').sum()
+        term1_tf, term1topN_tf = m.compute_tf_idf(dfoverall,dfbyparty_filt,500)
+    # %% save interim results
+    term1_tf.to_pickle(sys.argv[4*i+3])
+    term1topN_tf.to_csv(sys.argv[4*i+4])
