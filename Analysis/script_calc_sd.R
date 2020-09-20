9feb9cafb6314be41404296cbf7aed5a5315d341
culuc <luc.seiler@me.com>
2020-09-20 15:44:49 +0200

    test of accuracy with 2nd computation

--- Analysis/script_calc_sd.R
+++ Analysis/script_calc_sd.R
@@ -41,6 +41,7 @@ calc_sd_df <- function(df,data,dfacc){
     # print(dim(m$model5$trainingData))
 
     b <- matrix(NaN,5,100)
+    b2 <- matrix(NaN,5,100)
     n <- rep(NaN,5)
     accnull <- rep(NaN,5)
 
@@ -63,7 +64,7 @@ calc_sd_df <- function(df,data,dfacc){
             table.res <- table(factor(pr$predictions),factor(test))
             #
             b[i,j] <- sum(diag(table.res))/sum(table.res)
-
+            b2[i,j] <- confusionMatrix(factor(pr$predictions),factor(test))$Overall[['Accuracy']]
             # test2 <- factor(test)
             # pr2 <- factor(pr$predictions)
             #
@@ -80,6 +81,7 @@ calc_sd_df <- function(df,data,dfacc){
     }
     b_mean <-rowMeans(b)
     print(b)
+    print(b2)
     b_de.mean <- b - b_mean
     b_de.mean.sorted <- b_de.mean
     for (i in 1:5){
@@ -104,98 +106,6 @@ calc_sd_df <- function(df,data,dfacc){
 
  return(acc2)
 }
-
-# m <- readRDS(in_data)
-#
-# print(dim(m$model5$trainingData))
-#
-# b <- matrix(NaN,5,100)
-#
-# for (i in 1:5){
-#     for (j in 1:100){
-#         s<-m[[i]]$trainingData%>%
-#             sample_n(50,replace=T)
-#
-#         s<-predict(m[[i]]$preProcess,s)
-#
-#         pr<- predict(m[[i]],s%>%select(-c(.outcome)))
-#         test <- s$.outcome
-#         # pr<-droplevels(pr)
-#         # levels(test)<-levels(pr)
-#         # pr
-#         table.res <- table(factor(pr),factor(test))
-#         #
-#         b[i,j] <- sum(diag(table.res))/sum(table.res)
-#         # b[i]<-confusionMatrix(factor(pr),factor(test))[[3]][[1]]
-#         }
-#     }
-#
-#
-# b_mean <-rowMeans(b)
-#
-# b_de.mean <- b - b_mean
-# b_de.mean.sorted <- b_de.mean
-# for (i in 1:5){
-#     b_de.mean.sorted[i,] <- sort(b_de.mean[i,])
-# }
-#
-#
-#
-# # q <- matrix(NaN,5,2)
-# # print(q[1,])
-# # for (i in 1:5){
-# #     q[i,]<-quantile(b_de.mean.sorted[i,], c(0.05,0.95))
-# # }
-# #
-# # print(q)
-# #
-#
-# q2<- b_de.mean.sorted[,c(5,95)]
-#
-#
-# n <- rep(NaN,5)
-# for (i in 1:5){
-#     n[i]<-dim(m[[i]]$trainingData)[1]
-#     }
-#
-# q2<-q2*sqrt(50)/sqrt(n)
-#
-# qt2<-tibble('lb'=q2[,1],'ub'=q2[,2])
-#
-# # qt<-tibble('q05'=q[,1],'q95'=q[,2])
-# # q<-quantile(b_de.mean.sorted, c(0.05,0.95))
-# # q[[1]]
-# # q[[2]]
-# # q
-#
-# #
-# #
-# # ggparcoord(acc,
-# #     groupColumn='file',
-# #     columns = 2:ncol(acc),
-# #     scale='globalminmax'
-# #     )+
-# #     # theme_tufte(base_family='ArialMT')+
-# #     # scale_color_few()+
-# #     labs(x="Terms", y="Prediction Accuracy", col="Feature Selection")+
-# #     geom_errorbar(aes(ymin=len-sd, ymax=len+sd), width=.2,
-# #                    position=position_dodge(0.05))
-#
-# acc<- tibble('Term'=c('Term1','Term2','Term3','Term4','Term5'),'Prediction Accuracy'=rep(NaN,5))
-#
-# for (i in 1:5){
-#     acc[i,2] <- m[[i]]$result$Accuracy
-# }
-#
-#
-#
-
-# acc3 <- cbind(acc2,tibble('q95'=q[[2]]))
-# acc3 <- cbind(acc3,tibble('q05'=q[[1]]))
-# acc3 <- cbind(acc3,tibble('b_mean'=b_mean))
-# acc2 <- cbind(acc,qt2)
-# acc3<-acc3%>%add_column('terms'=rownames(acc3))
-
 
 
 labels     <- c('46th \n 1999\u20132003','47th \n 2003\u20132007','48th \n 2007\u20132011','49th \n 2011\u20132015','50th \n 2015\u20132019')
