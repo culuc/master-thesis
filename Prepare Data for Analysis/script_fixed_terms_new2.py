3cd6322dcdbcacfb97ce5e5ec95bb2be907c3bce
culuc <luc.seiler@me.com>
2020-09-25 21:23:09 +0200

    fix

--- Prepare Data for Analysis/script_fixed_terms_new2.py
+++ Prepare Data for Analysis/script_fixed_terms_new2.py
@@ -6,12 +6,22 @@ sys.path.append('../Modules')
 import modules as m
 import pandas as pd
 
+p1 = 'Sozialdemokratische Partei der Schweiz (SP)'
+p2 = 'Schweizerische Volkspartei (SVP)'
+p3 = 'FDP.Die Liberalen (FDP-Liberale)'
+p4 = 'Christlichdemokratische Volkspartei der Schweiz (CVP)'
+parties2 = [p1,p2]
+parties4 = [p1,p2,p3,p4]
+
 #%% snakemake.input[0]
 N = ast.literal_eval(sys.argv[1])
+n = ast.literal_eval(sys.argv[2])
 # allphr = pd.DataFrame()
 # allphr = set()
-for i in range(2,7):
+for i in range(3,8):
     df = pd.read_pickle(sys.argv[i])
+    if n is not None:
+        df = df[df['Speaker Party'].isin(parties4[:n])]
     if N is not None:
         df = df.groupby('Speaker Party')['tf_idf'].nlargest(N).reset_index(level=0,drop=True).reset_index()
     if i == 2:
@@ -22,4 +32,4 @@ print(len(allphr))
 allphr = pd.Series(list(allphr))
 allphr = pd.DataFrame(allphr, columns = ['Phrase'])
 allphr['Speaker Party'] = 0
-allphr.to_csv(sys.argv[7])
+allphr.to_csv(sys.argv[8])
